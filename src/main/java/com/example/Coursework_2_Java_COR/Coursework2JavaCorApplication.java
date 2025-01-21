3069f5c9dd4718dd1bdb8e93c985385e97982203
Nikolay <nikolayivanov944@gmail.com>
2025-01-21 23:22:18 +0300

    Init

---
+++ src/main/java/com/example/Coursework_2_Java_COR/Coursework2JavaCorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Coursework_2_Java_COR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2JavaCorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework2JavaCorApplication.class, args);
+	}
+
+}
